275383169ad08e218515b06e5e27dbc6e179ddc1
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2016-08-08 14:26:06 +0600

    first commit

---
+++ Computer Controller/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Computer_Controller
+{
+    partial class formController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroTabControl = new MetroFramework.Controls.MetroTabControl();
+            this.tab_timer = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage3 = new MetroFramework.Controls.MetroTabPage();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroTabControl.SuspendLayout();
+            this.tab_timer.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroTabControl
+            // 
+            this.metroTabControl.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.metroTabControl.Controls.Add(this.tab_timer);
+            this.metroTabControl.Controls.Add(this.metroTabPage1);
+            this.metroTabControl.Controls.Add(this.metroTabPage2);
+            this.metroTabControl.Controls.Add(this.metroTabPage3);
+            this.metroTabControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.metroTabControl.HotTrack = true;
+            this.metroTabControl.Location = new System.Drawing.Point(20, 60);
+            this.metroTabControl.MinimumSize = new System.Drawing.Size(10, 10);
+            this.metroTabControl.Name = "metroTabControl";
+            this.metroTabControl.SelectedIndex = 0;
+            this.metroTabControl.Size = new System.Drawing.Size(542, 100);
+            this.metroTabControl.TabIndex = 0;
+            this.metroTabControl.UseSelectable = true;
+            // 
+            // tab_timer
+            // 
+            this.tab_timer.Controls.Add(this.metroLabel3);
+            this.tab_timer.HorizontalScrollbarBarColor = true;
+            this.tab_timer.HorizontalScrollbarHighlightOnWheel = false;
+            this.tab_timer.HorizontalScrollbarSize = 10;
+            this.tab_timer.Location = new System.Drawing.Point(4, 4);
+            this.tab_timer.Name = "tab_timer";
+            this.tab_timer.Size = new System.Drawing.Size(534, 58);
+            this.tab_timer.TabIndex = 0;
+            this.tab_timer.Text = "  Timer  ";
+            this.tab_timer.VerticalScrollbarBarColor = true;
+            this.tab_timer.VerticalScrollbarHighlightOnWheel = false;
+            this.tab_timer.VerticalScrollbarSize = 10;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.metroLabel2);
+            this.metroTabPage1.Controls.Add(this.metroLabel1);
+            this.metroTabPage1.Controls.Add(this.metroTextBox1);
+            this.metroTabPage1.Controls.Add(this.metroButton1);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 4);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(534, 58);
+            this.metroTabPage1.TabIndex = 1;
+            this.metroTabPage1.Text = " Shutdown  ";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.HorizontalScrollbarSize = 10;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 4);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(534, 58);
+            this.metroTabPage2.TabIndex = 2;
+            this.metroTabPage2.Text = " Restart  ";
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            this.metroTabPage2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.VerticalScrollbarSize = 10;
+            // 
+            // metroTabPage3
+            // 
+            this.metroTabPage3.HorizontalScrollbarBarColor = true;
+            this.metroTabPage3.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage3.HorizontalScrollbarSize = 10;
+            this.metroTabPage3.Location = new System.Drawing.Point(4, 4);
+            this.metroTabPage3.Name = "metroTabPage3";
+            this.metroTabPage3.Size = new System.Drawing.Size(534, 58);
+            this.metroTabPage3.TabIndex = 3;
+            this.metroTabPage3.Text = "  Logoff  ";
+            this.metroTabPage3.VerticalScrollbarBarColor = true;
+            this.metroTabPage3.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage3.VerticalScrollbarSize = 10;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton1.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.metroButton1.Location = new System.Drawing.Point(250, 3);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(103, 52);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Start";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(31, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[] {
+        "0"};
+            this.metroTextBox1.Location = new System.Drawing.Point(106, 15);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(53, 23);
+            this.metroTextBox1.TabIndex = 3;
+            this.metroTextBox1.Text = "0";
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 19);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(97, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Shutdown after";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(165, 19);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(62, 19);
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "minute(s)";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(12, 10);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(83, 19);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.Text = "metroLabel3";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // formController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 172);
+            this.Controls.Add(this.metroTabControl);
+            this.Name = "formController";
+            this.Text = "Computer Controller";
+            this.metroTabControl.ResumeLayout(false);
+            this.tab_timer.ResumeLayout(false);
+            this.tab_timer.PerformLayout();
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl;
+        private MetroFramework.Controls.MetroTabPage tab_timer;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroTabPage metroTabPage3;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private System.Windows.Forms.Timer timer1;
+
+
+    }
+}
+
---
+++ Computer Controller/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using MetroFramework.Forms;
+
+namespace Computer_Controller
+{
+    public partial class formController : MetroForm
+    {
+        public formController()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, System.EventArgs e)
+        {
+            metroTabControl.SelectedTab = tab_timer;
+            timer1.Start();
+            metroLabel3.Text = metroTextBox1.Text;
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            var a = int.Parse(metroLabel3.Text);
+            a--;
+            metroLabel3.Text = a.ToString();
+        }
+    }
+}
